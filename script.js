0fbdf71705051105bcb77ac059c3ddf387f1353a
MellowMancer <yatharth.w@somaiya.edu>
2022-10-03 13:04:50 +0530

    Added functionality

--- script.js
+++ script.js
@@ -1,31 +1,6 @@
 const qArray = [
-    ["What is the capital of France?", "Paris", "London", "Berlin", "Rome"],
-    ["How many states are in the United States?", "50", "49", "51", "48"],
-    ["What is the capital of the United States?", "Washington, D.C.", "New York City", "Los Angeles", "Chicago"],
-    ["How many continents are there?", "7", "6", "8", "9"],
-    ["When did World War II end?", "1945", "1939", "1944", "1943"],
-    ["How many planets are in our solar system?", "8", "9", "7", "10"],
-    ["What is the largest country in the world?", "Russia", "China", "Canada", "United States"],
-    ["What is the smallest country in the world?", "Vatican City", "Monaco", "San Marino", "Nauru"],
-    ["What is the largest ocean in the world?", "Pacific Ocean", "Atlantic Ocean", "Indian Ocean", "Arctic Ocean"],
-    ["Where is the Great Barrier Reef located?", "Australia", "New Zealand", "Fiji", "Indonesia"],
-    ["What is the longest river in the world?", "Nile River", "Amazon River", "Yangtze River", "Mississippi River"],
-    ["What is the tallest mountain in the world?", "Mount Everest", "K2", "Kangchenjunga", "Lhotse"],
-    ["What is the largest desert in the world?", "Sahara Desert", "Gobi Desert", "Kalahari Desert", "Arabian Desert"],
-    ["What is the largest country in Africa?", "Algeria", "Nigeria", "Egypt", "South Africa"],
-    ["What is the largest country in South America?", "Brazil", "Argentina", "Colombia", "Peru"],
-    ["What is the largest country in Asia?", "China", "India", "Russia", "Indonesia"],
-    ["What is the largest country in Europe?", "Russia", "France", "Germany", "United Kingdom"],
-    ["How many countries are there in the world?", "195", "196", "197", "198"],
-    ["What is the largest city in the world?", "Tokyo", "Delhi", "Shanghai", "Sao Paulo"],
-    ["How many bones are there in the human body?", "206", "207", "208", "209"],
-    ["What is the largest organ in the human body?", "Skin", "Heart", "Lungs", "Liver"],
-    ["What is the smallest bone in tzsadasdasasdhe human body?", "Stapes", "Malleus", "Incus", "Tympanic Membrane"],
-    ["How many colors are there in a rainbow?", "7", "8", "9", "10"],
-    ["How many days are there in a leap year?", "366", "365", "364", "367"],
-    ["How many days are there in a non-leap year?", "365", "366", "364", "367"],
-]
-
+    [""]
+];
 
 function gen1() {
     let q = qArray[Math.floor(Math.random() * qArray.length)];
@@ -36,7 +11,7 @@ function gen1() {
     let q1o2 = document.getElementById("q1o2");
     let q1o3 = document.getElementById("q1o3");
     let q1o4 = document.getElementById("q1o4");
-    let timer1 = document.getElementById("timer1")
+    let timer1 = document.getElementById("timer1");
     timer1.innerHTML = "";
     q1num.innerHTML = qArray.indexOf(q) + 1 + ")";
     q1.innerHTML = q[0];
@@ -45,24 +20,35 @@ function gen1() {
     q1o3.innerHTML = q[3];
     q1o4.innerHTML = q[4];
     list1.style.listStyleType = "upper-alpha";
+
     setTimeout(() => {
-        timer1.style.border = "4px solid rgb(38, 255, 0)";
-    }, 1000);
-    let sec = 29;
+        timer1.style.border = "4px solid rgb(32, 255, 0)";
+    }, 1050);   
+    let sec = 39;
     let t1 = setInterval(function () {
         if (q1.innerHTML == q[0]) {
+            if (sec == -1) {
+                sec = 20;
+            }
+            if (sec<=9)
+            {
+                timer1.style.border = "4px solid rgb(232, 32, 232)";
+            }
+            else
+            {
+                timer1.style.border = "4px solid rgb(32, 255, 0)";
+            }
             timer1.innerHTML = sec;
             sec--;
-            if (sec == -1) {
-                sec = 15;
-            }
-        } else {
+        } 
+        else {
             clearInterval(t1);
         }
-    }, 1000);
+    }, 1050);
 
 
     let s1 = setTimeout(() => {
+        
         if (q1.innerHTML == q[0]) {
             q1num.innerHTML = "";
             q.innerHTML = "";
@@ -74,11 +60,12 @@ function gen1() {
             timer1.innerHTML = "";
             list1.style.listStyleType = "none";
             timer1.style.border = "none";
+            
         }
         else {
             clearTimeout(s1);
         }
-    }, 47000);
+    }, 65100);
 }
 
 function gen2() {
@@ -90,12 +77,9 @@ function gen2() {
     let q2o2 = document.getElementById("q2o2");
     let q2o3 = document.getElementById("q2o3");
     let q2o4 = document.getElementById("q2o4");
-    let timer2 = document.getElementById("timer2")
+    let timer2 = document.getElementById("timer2");
     timer2.innerHTML = "";
-    setTimeout(() => {
-        timer2.style.border = "4px solid rgb(38, 255, 0)";
-    }, 1000);
-    q2num.innerHTML = qArray.indexOf(q) + 1;
+    q2num.innerHTML = qArray.indexOf(q) + 1 + ")";
     q2.innerHTML = q[0];
     q2o1.innerHTML = q[1];
     q2o2.innerHTML = q[2];
@@ -103,20 +87,32 @@ function gen2() {
     q2o4.innerHTML = q[4];
     list2.style.listStyleType = "upper-alpha";
 
-    let sec = 29;
+    setTimeout(() => {
+        timer2.style.border = "4px solid rgb(32, 255, 0)";
+    }, 1050);   
+    let sec = 39;
     let t2 = setInterval(function () {
         if (q2.innerHTML == q[0]) {
+            if (sec == -1) {
+                sec = 20;
+            }
+            if (sec<=9){
+                timer2.style.border = "4px solid rgb(255, 32, 32)";
+            }
+            else{
+                timer2.style.border = "4px solid rgb(32, 255, 0)";
+            }
             timer2.innerHTML = sec;
             sec--;
-            if (sec == -1) {
-                sec = 15;
-            }
-        } else {
+        } 
+        else {
             clearInterval(t2);
         }
-    }, 1000);
+    }, 1050);
+
 
     let s2 = setTimeout(() => {
+        
         if (q2.innerHTML == q[0]) {
             q2num.innerHTML = "";
             q.innerHTML = "";
@@ -128,11 +124,12 @@ function gen2() {
             timer2.innerHTML = "";
             list2.style.listStyleType = "none";
             timer2.style.border = "none";
+            
         }
         else {
-            clearTimeout(s2);
+            clearTimeout(s1);
         }
-    }, 47000);
+    }, 65100);
 }
 
 function gen3() {
@@ -144,31 +141,42 @@ function gen3() {
     let q3o2 = document.getElementById("q3o2");
     let q3o3 = document.getElementById("q3o3");
     let q3o4 = document.getElementById("q3o4");
-    let timer3 = document.getElementById("timer3")
+    let timer3 = document.getElementById("timer3");
     timer3.innerHTML = "";
-    setTimeout(() => {
-        timer3.style.border = "4px solid rgb(38, 255, 0)";
-    }, 1000);
-    q3num.innerHTML = qArray.indexOf(q) + 1;
+    q3num.innerHTML = qArray.indexOf(q) + 1 + ")";
     q3.innerHTML = q[0];
     q3o1.innerHTML = q[1];
     q3o2.innerHTML = q[2];
     q3o3.innerHTML = q[3];
     q3o4.innerHTML = q[4];
     list3.style.listStyleType = "upper-alpha";
-    let sec = 29;
+
+    setTimeout(() => {
+        timer3.style.border = "4px solid rgb(32, 255, 0)";
+    }, 1050);   
+    let sec = 39;
     let t3 = setInterval(function () {
         if (q3.innerHTML == q[0]) {
+            if (sec == -1) {
+                sec = 20;
+            }
+            if (sec<=9){
+                timer3.style.border = "4px solid rgb(255, 32, 32)";
+            }
+            else{
+                timer3.style.border = "4px solid rgb(32, 255, 0)";
+            }
             timer3.innerHTML = sec;
             sec--;
-            if (sec == -1) {
-                sec = 15;
-            }
-        } else {
+        } 
+        else {
             clearInterval(t3);
         }
-    }, 1000);
+    }, 1050);
+
+
     let s3 = setTimeout(() => {
+        
         if (q3.innerHTML == q[0]) {
             q3num.innerHTML = "";
             q.innerHTML = "";
@@ -180,11 +188,12 @@ function gen3() {
             timer3.innerHTML = "";
             list3.style.listStyleType = "none";
             timer3.style.border = "none";
+            
         }
         else {
             clearTimeout(s3);
         }
-    }, 47000);
+    }, 65100);
 }
 
 function gen4() {
@@ -196,31 +205,42 @@ function gen4() {
     let q4o2 = document.getElementById("q4o2");
     let q4o3 = document.getElementById("q4o3");
     let q4o4 = document.getElementById("q4o4");
-    let timer4 = document.getElementById("timer4")
+    let timer4 = document.getElementById("timer4");
     timer4.innerHTML = "";
-    setTimeout(() => {
-        timer4.style.border = "4px solid rgb(38, 255, 0)";
-    }, 1000);
-    q4num.innerHTML = qArray.indexOf(q) + 1;
+    q4num.innerHTML = qArray.indexOf(q) + 1 + ")";
     q4.innerHTML = q[0];
     q4o1.innerHTML = q[1];
     q4o2.innerHTML = q[2];
     q4o3.innerHTML = q[3];
     q4o4.innerHTML = q[4];
     list4.style.listStyleType = "upper-alpha";
-    let sec = 29;
+
+    setTimeout(() => {
+        timer4.style.border = "4px solid rgb(32, 255, 0)";
+    }, 1050);   
+    let sec = 39;
     let t4 = setInterval(function () {
         if (q4.innerHTML == q[0]) {
+            if (sec == -1) {
+                sec = 20;
+            }
+            if (sec<=9){
+                timer4.style.border = "4px solid rgb(255, 32, 32)";
+            }
+            else{
+                timer4.style.border = "4px solid rgb(32, 255, 0)";
+            }
             timer4.innerHTML = sec;
             sec--;
-            if (sec == -1) {
-                sec = 15;
-            }
-        } else {
+        } 
+        else {
             clearInterval(t4);
         }
-    }, 1000);
+    }, 1050);
+
+
     let s4 = setTimeout(() => {
+        
         if (q4.innerHTML == q[0]) {
             q4num.innerHTML = "";
             q.innerHTML = "";
@@ -232,9 +252,10 @@ function gen4() {
             timer4.innerHTML = "";
             list4.style.listStyleType = "none";
             timer4.style.border = "none";
+            
         }
         else {
             clearTimeout(s4);
         }
-    }, 47000);
+    }, 65100);
 }
